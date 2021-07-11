441b30523828b309798226f32a357bff9eeb8150
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2021-07-12 02:54:58 +0530

    feat: Update  site config file

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,14 +16,35 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["link", { rel: "icon", href: "/logo.png" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#2e42ef" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     [
       "meta",
       { name: "apple-mobile-web-app-status-bar-style", content: "black" },
     ],
+    [
+      "link",
+      { rel: "apple-touch-icon", href: "/icons/apple-touch-icon-152x152.png" },
+    ],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/icons/safari-pinned-tab.svg",
+        color: "#2e42ef",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "msapplication-TileImage",
+        content: "/icons/msapplication-icon-144x144.png",
+      },
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }],
   ],
-
   /**
    * Theme configuration, here is the default theme configuration for VuePress.
    *
@@ -31,10 +52,12 @@ module.exports = {
    */
   themeConfig: {
     logo: "/logo.png",
-    repo: "",
-    editLinks: false,
-    docsDir: "",
-    editLinkText: "",
+    smoothScroll: true,
+    repo: "uiuxarghya/project-xii",
+    docsDir: "docs",
+    docsBranch: "main",
+    editLinks: true,
+    editLinkText: "Edit this page on GitHub",
     lastUpdated: true,
     nextLinks: true,
     // default value is true. Set it to false to hide prev page links on all pages
@@ -48,10 +71,6 @@ module.exports = {
         text: "Subjects",
         ariaLabel: "Subjects Menu",
         items: [{ text: "Chemistry", link: "/chemistry/" }],
-      },
-      {
-        text: "GitHub",
-        link: "https://github.com/uiuxarghya/project-xii",
       },
       {
         text: "About",
@@ -85,15 +104,27 @@ module.exports = {
       ],
     },
   },
-
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
   plugins: [
-    "@vuepress/theme-default",
-    "@vuepress/search",
-    {
-      searchMaxSuggestions: 10,
-    },
-  ]["@vuepress/back-to-top"],
+    ["@vuepress/theme-default"],
+    [
+      "@vuepress/search",
+      {
+        searchMaxSuggestions: 10,
+      },
+    ],
+    ["@vuepress/back-to-top"],
+    [
+      ("@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "New content is available.",
+          buttonText: "Refresh",
+        },
+      }),
+    ],
+  ],
 };
